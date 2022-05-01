64a56b5f122197598dc8d49998659fbf5b6c59d2
Fernando Aponte <fernandoaponte90@gmail.com>
2022-04-30 22:01:43 -0400

    custumer and pet service

---
+++ src/modules/custumers/custumers.service.ts
@@ -0,0 +1,42 @@
+import { Model, Types } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+
+import { Injectable } from '@nestjs/common';
+import { CreateCustumerDto } from './dto/create-custumer.dto';
+import { UpdateCustumerDto } from './dto/update-custumer.dto';
+
+import { Custumer, CustumerDocument } from '../../schemas/custumer.schema';
+
+@Injectable()
+export class CustumersService {
+
+  constructor(@InjectModel(Custumer.name) private custumerModel: Model<CustumerDocument>) { }
+
+
+  create(createCustumerDto: CreateCustumerDto): Promise<Custumer> {
+    // return 'This action adds a new custumer';
+    const createdCustumer = new this.custumerModel(createCustumerDto);
+    return createdCustumer.save();
+  }
+
+  findAll() {
+    // return `This action returns all custumers`;
+    return this.custumerModel.find();
+  }
+
+  findOne(id: string) {
+    // return `This action returns a #${id} custumer`;
+    return this.custumerModel.findOne({ custumer: new Types.ObjectId(id) });
+  }
+
+  update(id: string, updateCustumerDto: UpdateCustumerDto) {
+    // return `This action updates a #${id} custumer`;
+    return this.custumerModel.updateOne(
+      { custumer: new Types.ObjectId(id) },
+      { $set: updateCustumerDto });
+  }
+
+  remove(id: string) {
+    return `This action removes a #${id} custumer`;
+  }
+}
