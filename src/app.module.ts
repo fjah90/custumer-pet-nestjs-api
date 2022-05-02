8f4fd49a4f3fe9c31991ffe10a371968733d6b34
Fernando Aponte <fernandoaponte90@gmail.com>
2022-05-02 16:40:02 -0400

    setting heroko deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,8 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { PetsModule } from './modules/pets/pets.module';
 import { CustumersModule } from './modules/custumers/custumers.module';
 
-const uri = 'mongodb://localhost:27017/petApp';
+const uri = 'mongodb+srv://petapp:XAANlb5scTTLPN4f@cluster0.d7ukh.mongodb.net/petapp';
+// const uri = 'mongodb://localhost:27017/petApp';
 
 @Module({
   imports: [MongooseModule.forRoot(process.env.MONGODB_STORE_URI || uri), PetsModule, CustumersModule],
